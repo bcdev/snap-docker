b09797a829411135035e8a564a8729f69ef27f32
Olaf Danne <olaf.danne@brockmann-consult.de>
2025-04-11 14:33:07 +0200

    continued Dockerfile

--- snap12/install-miniforge.bash
+++ snap12/install-miniforge.bash
@@ -6,12 +6,7 @@ cd $(dirname $0)
 #MAMBAFORGE_VERSION=4.9.2-3
 # Python 3.12, Oct 2024:
 MAMBAFORGE_VERSION=24.9.0-0
-# SHA256 for installers can be obtained from https://github.com/conda-forge/miniforge/releases
-SHA256SUM="2263861c8927fce272fd97072e1822f268c6bec8c2fee9724d7df54d7fe80a36"
-#https://github.com/conda-forge/miniforge/releases/download/4.9.2-3/Mambaforge-4.9.2-3-Linux-x86_64.sh
-
 #URL="https://github.com/conda-forge/miniforge/releases/download/${MAMBAFORGE_VERSION}/Mambaforge-${MAMBAFORGE_VERSION}-Linux-x86_64.sh"
-#    https://github.com/conda-forge/miniforge/releases/download/24.11.3-2/Miniforge3-24.11.3-2-Linux-x86_64.sh
 URL="https://github.com/conda-forge/miniforge/releases/download/${MAMBAFORGE_VERSION}/Miniforge3-${MAMBAFORGE_VERSION}-Linux-x86_64.sh"
 INSTALLER_PATH=/tmp/miniforge-installer.sh
 
@@ -22,19 +17,12 @@ unset HOME
 wget --quiet --no-check-certificate $URL -O ${INSTALLER_PATH}
 chmod +x ${INSTALLER_PATH}
 
-# check sha256 checksum
-#if ! echo "${SHA256SUM}  ${INSTALLER_PATH}" | sha256sum  --quiet -c -; then
-#    echo "sha256 mismatch for ${INSTALLER_PATH}, exiting!"
-#    exit 1
-#fi
-
 bash ${INSTALLER_PATH} -b -p ${CONDA_DIR}
 export PATH="${CONDA_DIR}/bin:$PATH"
 
 # Preserve behavior of miniconda - packages come from conda-forge + defaults
 conda config --system --append channels conda-forge
 conda config --system --append channels defaults
-conda config --system --append channels terradue
 conda config --system --append channels r
 
 # Do not attempt to auto update conda or dependencies
